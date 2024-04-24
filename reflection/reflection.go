f1c3b7e935be82bfbf599ac3c8be2ec11faaf927
Rubén S. García <rssanjuan2704@gmail.com>
2024-04-24 23:10:17 +0200

    ch11: Incorrect abstraction (oh no!)

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,9 +1,41 @@
 package reflection
 
-import "reflect"
+import (
+	"reflect"
+)
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+	val := getValue(x)
+
+	numberOfValues := 0
+	var getField func(int) reflect.Value
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Slice, reflect.Array:
+		numberOfValues = val.Len()
+		getField = val.Index
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walk(val.MapIndex(key).Interface(), fn)
+		}
+	}
+
+	for i := 0; i < numberOfValues; i++ {
+		walk(getField(i).Interface(), fn)
+	}
+}
+
+func getValue(x interface{}) (val reflect.Value) {
+	val = reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return
 }
