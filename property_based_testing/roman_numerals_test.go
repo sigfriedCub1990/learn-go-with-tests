97f7c12a50dfe11f5ebdc6c337c94a861b0e6561
Rubén S. García <rssanjuan2704@gmail.com>
2024-04-30 22:17:45 +0200

    feat: Roman numerals

---
+++ property_based_testing/roman_numerals_test.go
@@ -0,0 +1,32 @@
+package propertybasedtesting
+
+import "testing"
+
+func TestRomanNumerals(t *testing.T) {
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
+		{"6 gets converted to VI", 6, "VI"},
+		{"7 gets converted to VII", 7, "VII"},
+		{"8 gets converted to VIII", 8, "VIII"},
+		{"9 gets converted to IX", 9, "IX"},
+		{"10 gets converted to X", 10, "X"},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
+	}
+}
