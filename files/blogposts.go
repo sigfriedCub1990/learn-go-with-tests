ab8d39be9e34028b519340ef522db13f9db32c4b
Rubén S. García <rssanjuan2704@gmail.com>
2024-05-12 19:49:33 +0200

    feat: Build post with Title

--- files/blogposts.go
+++ files/blogposts.go
@@ -1,10 +1,12 @@
 package blogposts
 
 import (
+	"io"
 	"io/fs"
 )
 
 type Post struct {
+	Title string
 }
 
 func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
@@ -13,8 +15,32 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 		return nil, err
 	}
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
 	}
 	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	return newPost(postFile)
+}
+
+func newPost(f fs.File) (Post, error) {
+	postData, err := io.ReadAll(f)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
 }
--- files/blogposts_test.go
+++ files/blogposts_test.go
@@ -3,6 +3,7 @@ package blogposts_test
 import (
 	"errors"
 	"io/fs"
+	"reflect"
 	"testing"
 	"testing/fstest"
 
@@ -17,20 +18,23 @@ func (s StubFailingFs) Open(name string) (fs.File, error) {
 }
 
 func TestNewBlogPosts(t *testing.T) {
-	t.Run("read files from filysyste", func(t *testing.T) {
+	t.Run("read files from filesystem", func(t *testing.T) {
 		fs := fstest.MapFS{
-			"hellow-world.md":  {Data: []byte("hi")},
-			"hellow-world2.md": {Data: []byte("hola")},
+			"hellow-world.md":  {Data: []byte("Title: Post 1")},
+			"hellow-world2.md": {Data: []byte("Title: Post 2")},
 		}
 
 		posts, err := blogposts.NewPostsFromFS(fs)
 
 		if err != nil {
-			t.Fatal("this should fail")
+			t.Fatal("this should not fail")
 		}
 
-		if len(posts) != len(fs) {
-			t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+		got := posts[0]
+		want := blogposts.Post{Title: "Post 1"}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %+v, want %+v", got, want)
 		}
 	})
 
